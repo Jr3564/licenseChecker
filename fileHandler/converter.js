9e7f6c9a8046b743192a675475995b647ecdd43b
Rodolfo <rodolfojr94@gmail.com>
2022-02-03 08:34:53 -0300

    refactor: Implemented Clean Code concepts.

--- fileHandler/converter.js
+++ fileHandler/converter.js
@@ -1,21 +1,25 @@
-const endCutter = (string) => string.slice(1, -1);
+const removeQuotes = (word) =>
+  word[0] === "'" || word[0] === '"' ? word.slice(1, -1) : word;
+
+const addQuotes = (word, quote = '"') =>
+  word[0] === quote ? word : quote + word + quote;
+
+const capitalize = (word) => word[0].toUpperCase() + word.substr(1);
+
+const toCamelCase = (keyWithSpaces) => {
+  const words = keyWithSpaces.split(" ");
+  const firstWord = words.shift().toLowerCase();
+  return words.reduce((acc, key) => acc + capitalize(key), firstWord);
+};
 
 exports.csvToObject = (licensesCSV) => {
   const lines = licensesCSV.split("\n").filter((line) => !!line);
-  const keys = lines.shift().split(",");
+  const fistLinekeys = lines.shift().split(",");
+  const keys = fistLinekeys.map((key) => toCamelCase(removeQuotes(key)));
 
-  const jsKeysDefaults = keys.map((key) => {
-    const keyArr = key.split(" ");
-    const newKey =
-      keyArr.length < 2
-        ? keyArr[0].toLowerCase()
-        : keyArr[0].toLowerCase() + keyArr[1];
-    return endCutter(newKey);
-  });
-
-  const result = lines.map((column) => {
-    return column.split(",").reduce((acc, key, index) => {
-      return { ...acc, [jsKeysDefaults[index]]: endCutter(key) };
+  const result = lines.map((line) => {
+    return line.split(",").reduce((acc, value, index) => {
+      return { ...acc, [keys[index]]: removeQuotes(value) };
     }, {});
   });
 
@@ -24,16 +28,27 @@ exports.csvToObject = (licensesCSV) => {
 
 exports.jsonToObject = (licensesJson) => JSON.parse(licensesJson);
 
+const extractValuesAccording = (keys, values) => [
+  ...keys.map((key) => values[key]),
+];
+
+const arrayToStringsLine = (array, valuesQuotes = "") =>
+  array.reduce((stringLine, value, index) => {
+    const commaOrLineBreak = index + 1 < array.length ? "," : "\n";
+    const valueWithQuotes = valuesQuotes
+      ? addQuotes(value, valuesQuotes)
+      : value;
+    return stringLine + valueWithQuotes + commaOrLineBreak;
+  }, "");
+
 exports.objectToCSV = (licences) => {
-  const keys = Object.keys(licences[0]);
-  const linesColumns = licences.reduce(
-    (acc, crr) => {
-      return [...acc, [...keys.map((key) => crr[key])]];
+  const fistLinekeys = Object.keys(licences[0]);
+  return licences.reduce(
+    (lines, line) => {
+      const arrayLine = extractValuesAccording(fistLinekeys, line);
+      const stringLine = arrayToStringsLine(arrayLine, '"');
+      return [...lines, stringLine];
     },
-    [keys]
+    [arrayToStringsLine(fistLinekeys, '"')]
   );
-  return linesColumns.map((line) => {
-    const values = line.map((value) => `"${value}"`);
-    return values.join(",") + "\n";
-  });
 };
